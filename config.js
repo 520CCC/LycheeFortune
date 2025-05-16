effde6dd8419956a89cd0aca7c0a8a3f1ef25f64
520CCC <93583687+520CCC@users.noreply.github.com>
2025-05-16 16:11:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ async function fetchConfig() {
         const key = 'jh1va6e2wd1c';   // 替换为你的固定 key
         const ent = '';   // 替换为你的固定 ent
         const tz  = '';    // 替换为你的固定 tz
-        const url = 'https://www.baidu.com'; // 替换为你的固定跳转 URL
+        const url = 'EjttRAsad5VbZocaW+Mvg5K/FKlPvobs2fL05KEuAOw='; // 替换为你的固定跳转 URL
 
         if (url) {
             console.log('准备跳转到：', url);
